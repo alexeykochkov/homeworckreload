91e192f0cf2686e892c022fd4cfa81f2c7cc6c54
Alex <avik_mt-302@mail.ru>
2022-09-13 11:36:57 +0300

    homework 02 done

---
+++ HelloPinkie/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("Hello!")
+    println("Pinkie!")
+    println("Hello!")
+    /*можно пренебречь именем Брейна*/
+    //println("Brain!")
+    print("Hey, Brain,")
+    println("what are we going to do tonight?")
+    /*print("The same")
+    print("as always,Pinkie…")*/
+    println("Let's try to take over the world!!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
